197d2c9a4698a94e7bdc733ff761585c67ee1e42
soulivanh vorlasane <soulivanh@soulivanhs-MacBook-Pro.local>
2026-01-09 09:58:31 +0700

    fisrt create rest api

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Rest API",
+    "version": "1.0",
+    'author': 'soulivanh',
+    'website': 'https://ifundev.com',
+    "summary": "Rest API",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        # "views/res_users_views.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
